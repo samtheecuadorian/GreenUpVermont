01f18875fabe9e64fd3bf6ac401fdc950a102e6b
John Need <john.need@gmail.com>
2017-06-04 18:08:23 -0400

    Google Signin working on Android

--- index.android.js
+++ index.android.js
@@ -14,9 +14,19 @@ import {
     Text,
     View
 } from 'react-native';
+import * as firebase from 'firebase';
 import FireAuth from 'react-native-firebase-auth';
 import logo from './assets/GreenupVermontlogo.png';
-
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyAjwSCpOvLPgYcFr26V3gmfwJlGb-VtWAs",
+  authDomain: "greenupvermont-de02b.firebaseapp.com",
+  databaseURL: "https://greenupvermont-de02b.firebaseio.com",
+  projectId: "greenupvermont-de02b",
+  storageBucket: "greenupvermont-de02b.appspot.com",
+  messagingSenderId: "439621369113"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 const onButtonPress = () => {
     Alert.alert('Button has been pressed!');
 };
@@ -40,17 +50,94 @@ const styles = StyleSheet.create({
     },
 });
 
+
+function register() {
+    const {email, password, firstName, lastName} = this.state;
+    FireAuth.register(email, password, {firstName, lastName});
+}
+
+function login() {
+    FireAuth.login(this.state.email, this.state.password);
+}
+
+function facebookLogin() {
+    FireAuth.facebookLogin();
+}
+
+function googleLogin() {
+    Alert.alert('GOOGLE LOGIN PRESSED');
+    FireAuth.googleLogin();
+
+}
+
+function logout() {
+    FireAuth.logout();
+}
+
+function update() {
+    FireAuth.update({
+        firstName: this.state.firstName,
+        lastName: this.state.lastName
+    }).then(() => {
+        Alert.alert('UPDATE SUCCESS');
+    }).catch(err => {
+        Alert.alert('UPDATE FAIL');
+    });
+}
+
+function resetPassword() {
+    FireAuth.resetPassword(this.state.email)
+        .then(() => {
+
+        })
+        .catch(err => {
+
+        });
+}
+
+function updatePassword() {
+    FireAuth.updatePassword(this.state.password)
+        .then(() => {
+
+        })
+        .catch(err => {
+
+        });
+};
+
+function onLogin() {
+}
+function onUserChange() {
+}
+function onLogout() {
+}
+function emailVerified() {
+}
+function onError() {
+}
+
+
 export default class GreenUpVermont extends Component {
-  constructor(props) {
-    super(props);
-    FireAuth.init({iosClientId: <IOS_CLIENT_ID>}); // This is the CLIENT_ID found in your Google services plist.
-  }
+    constructor(props) {
+        super(props);
+        this.onLogin = onLogin.bind(this);
+        this.onUserChange = onUserChange.bind(this);
+        this.onLogout = onLogout.bind(this);
+        this.emailVerified = emailVerified.bind(this);
+        this.onError = onError.bind(this);
+        FireAuth.init({iosClientId: "blahblahblah"}); // This is the CLIENT_ID found in your Google services plist.
+    }
+
+    componentDidMount() {
+        FireAuth.setup(this.onLogin, this.onUserChange, this.onLogout, this.emailVerified, this.onError);
+    }
+
 
     render() {
         return (
             <View style={styles.container}>
                 <Image
-                    source={logo} />
+                    source={logo}/>
                 <Text style={styles.welcome}>
                     Welcome to the Green Up Vermont App!
                 </Text>
@@ -62,7 +149,7 @@ export default class GreenUpVermont extends Component {
                     Shake or press menu button for dev menu
                 </Text>
                 <Button
-                    onPress={onButtonPress}
+                    onPress={googleLogin}
                     title="Login with Google"
                     accessibilityLabel="Login With Google"
                 />
@@ -70,7 +157,6 @@ export default class GreenUpVermont extends Component {
         );
     }
 }
-
 
 
 AppRegistry.registerComponent('GreenUpVermont', () => GreenUpVermont);
